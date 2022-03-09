c4dec873e86571d96c833770c9aed62a8cced65c
sugukub <50629834+sugukub@users.noreply.github.com>
2022-03-09 15:22:37 +0530

    Url file changes

--- Instagram_page_stats/urls.py
+++ Instagram_page_stats/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("page_stats/", include("page_stats.urls"))
+    path("page_stats/", include("page_stats.urls")),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}) 
     ]
